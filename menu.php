d67f5c53baf5dd85f8efc89d9fc355bf0e004cb6
grosset-floriane <fl.grosset@gmail.com>
2014-10-11 17:37:47 +0200

    New files

---
+++ menu.php
@@ -0,0 +1,20 @@
+<div class="sommaire">
+
+                <header class="header">
+                    <a href="index.php?r=welcome"><img class="logo" src="logo.png" alt=""/></a>
+                </header>
+
+                <nav>
+                    <ul class="nav">
+                        <li class="texte">Galerie Web gérée par des étudiants en deuxième année à l'Ecole Supérieure d'Art et Média de Caen.</li>
+                        <li class="lien"><a class="lien" href="index.php?r=welcome">Accueil</a></li>
+                        <li class="lien"><a class="lien" href="index.php?r=archives">Archives</a></li>
+                        <li class="lien"><a class="lien" href="index.php?r=casier">Casier itinérant</a></li>
+                        <li class="lien"><a class="lien" href="index.php?r=about"> À propos</a></li>
+                        <li class="lien"><a class="lien" href="index.php?=contact">Contact</a></li>
+                        <li class="lien"><a class="lien break" href="index.php?r=external_link">Liens externes</a></li>
+
+                    </ul>
+
+                </nav>
+            </div>
---
+++ menu_organique.php
@@ -0,0 +1,9 @@
+<aside class="bloc_icones">
+    <a href="index.php?r=cellules_mort"><img class="icones"src="" alt="Cellules de mort"></a>
+    <a href="index.php?r=ecorche"><img class="icones"src="" alt="Ecorché"></a>
+    <a href="index.php?r=racines"><img class="icones"src="" alt="Racines"></a>
+    <a href="index.php?r=phaeophyceae"><img class="icones"src="" alt="Phaeophyceae"></a>
+    <a href="index.php?r=a_travers"><img class="icones"src="" alt="A travers"></a>
+    <a href="index.php?r=arbrologie"><img class="icones"src="" alt="Arbrologie"></a>
+    <a href="index.php?r=lun_lautre"><img class="icones"src="" alt="L'un dans l'autre"></a>
+</aside>
